ecf50d8e1d39ea3e707611ba6e0dc267b463165a
michaelrambeau <mikeair@gmail.com>
2016-10-31 06:14:31 +0900

    Add pagination for '/users', '/shipments' and '/orders' routes

--- src/routes/Users/components/User.js
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react'
-import User from 'components/User'
-
-export default ({ user, children }) => (
-  <section className="section">
-    <User user={user} />
-    { children }
-  </section>
-)
