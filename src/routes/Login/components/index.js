47a4c89918a521efb011852742d000bd952ba1c8
michaelrambeau <mikeair@gmail.com>
2016-11-11 07:33:14 +0900

    Auth URL (still WIP)

--- src/routes/Login/components/index.js
+++ src/routes/Login/components/index.js
@@ -4,13 +4,13 @@ import config from 'store/config'
 const APP_URL = 'https://shipandco.auth0.com'
 const client_id = '3w7EXmzzpZ4dkQGQuO6yhYAIRbLrQVIb'
 // const client_id = 'DUuPWkiKhQexHjZbpVO7b9wKMj2f7tsq'
-const redirect_uri = `${self.location.origin}/auth0.html`
+// const redirect_uri = `${self.location.origin}/auth0.html`
+const redirect_uri = `http://localhost:3030/login/callback?source=${location.href}`
 // const auth0Client = 'eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiNi44LjAifQ'
-// const url = `${APP_URL}/authorize?scope=openid&response_type=token&connection=google-oauth2&sso=true&client_id=${client_id}&redirect_uri=${redirect_uri}`
+const url = `${APP_URL}/authorize?scope=openid&response_type=token&connection=google-oauth2&sso=true&client_id=${client_id}&redirect_uri=${redirect_uri}`
 
-const url = 'http://localhost:3030/auth/auth0'
-
-
+// const url = 'http://localhost:3030/auth/auth0'
+// const url = `http://localhost:3030/login/request?source=${location.href}`
 
 export default () => (
   <section className="section">
