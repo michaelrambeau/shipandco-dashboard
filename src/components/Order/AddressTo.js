f84a260d8a6070da5671cab17054b69b29196a49
michaelrambeau <mikeair@gmail.com>
2017-08-21 13:11:34 +0900

    Fix shipping address name

--- src/components/Order/AddressTo.js
+++ src/components/Order/AddressTo.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import Flag from 'components/utils/Flag'
 
-const Item = ({ label, children }) => (
+const Item = ({ label, children }) =>
   <div style={{ marginBottom: '1rem' }}>
-    <label className="label">{label}</label>
+    <label className="label">
+      {label}
+    </label>
     <p className="control">
       {children}
     </p>
   </div>
-)
 
 const Empty = () => <span className="empty">(empty)</span>
 
-const To = ({ address }) => (
+const To = ({ address }) =>
   <div>
     <Item label="Name">
-      {address.name || <Empty />}
+      {address.full_name || address.name || <Empty />}
     </Item>
     <Item label="Address #1">
       {address.address1 || <Empty />}
-      </Item>
+    </Item>
     <Item label="Address #2">
       {address.address2 || <Empty />}
     </Item>
@@ -27,17 +28,14 @@ const To = ({ address }) => (
       {address.city || <Empty />}
     </Item>
     <Item label="Zip Code">
-      {address.zip || <Empty /> }
+      {address.zip || <Empty />}
     </Item>
     <Item label="Province Code">
       {address.province_code || <Empty />}
     </Item>
     <Item label="Country">
-      <Flag countryCode={address.country_code} />
-      {' '}
-      {address.country}
+      <Flag countryCode={address.country_code} /> {address.country}
     </Item>
   </div>
-)
 
 export default To
